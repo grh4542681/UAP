57413240f98cf77ac05acb435f7d2720fafa4589
Ronghua Gao <grh4542681@163.com>
2019-04-06 05:50:06 +0200

    process

--- plat/infra/process/single/process_single.h
+++ plat/infra/process/single/process_single.h
@@ -1,12 +1,39 @@
 #ifndef _PROCESS_SINGLE_H__
 #define _PROCESS_SINGLE_H__
 
+#include <unistd.h>
+
+#include "process_info.h"
+
 namespace process::single {
 
+template < typename F >
 class ProcessSingle {
 public:
     ProcessSingle();
     ~ProcessSingle();
+
+    template <typename ... Args>
+    ProcessRet Run(Args&& ... args) {
+        pid_t pid = fork()
+        if (pid < 0) {
+        
+        } else if (pid = 0) {
+
+        } else {
+
+        }
+        return Process:SUCCESS;
+    }
+
+private:
+    ProcessInfo* process_info_;
+    F func_;
+
+    template <typename ... Args>
+    ProcessRet _run_main(Args&& ... args) {
+
+    }
 };
 
 };
