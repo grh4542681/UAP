494d3fe71578c3ddfa420eeee51f999b0020d719
Ronghua Gao <grh4542681@163.com>
2019-04-11 10:25:40 +0200

    time c

---
+++ plat/infra/util/time/time_c.cc
@@ -0,0 +1,26 @@
+#include "time_c.h
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <time.h>
+#endif
+
+int 64_t GetSysTimeMicros()
+{
+
+}
+
+
+
+
+
+
+void GetCurrentTime()
+{
+     /**/
+     util::TimeC date;
+     time_t now;
+     localtime_s()
+
+}
---
+++ plat/infra/util/time/time_c.h
@@ -0,0 +1,20 @@
+#ifndef __TIME_C_H__
+#define __TIME_C_H__
+
+namespace util::time {
+
+class TimeC {
+
+private:
+      int time_sec_;
+      int time_min_;
+      int time_hour_;
+      int time_day_;
+      int time_mon_;
+      int time_year_;
+
+};
+
+}
+
+#endif
