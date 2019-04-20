5ccaaa5dc7b20477d79429bcbeab6ba70cc586e3
Ronghua Gao <grh4542681@163.com>
2019-04-20 11:12:32 +0200

    shm

---
+++ plat/infra/process/doxygen_help.h
@@ -0,0 +1,24 @@
+/**
+ * @namespace process
+ * @brief process operator main namespace.
+ */
+
+/**
+ * @namespace process::signal
+ * @brief process signal operator namespace.
+ */
+
+/**
+ * @namespace process::singal
+ * @brief singal process operator namespace.
+ */
+
+/**
+ * @namespace process::mutex
+ * @brief process level mutex lock namespace.
+ */
+
+/**
+ * @namespace process::pool
+ * @brief process pool namespace.
+ */
---
+++ plat/infra/util/doxygen_help.h
@@ -0,0 +1,9 @@
+/**
+ * @namespace util
+ * @brief utils main namespace.
+ */
+
+/**
+ * @namespace util::time
+ * @brief time operator tools namespace.
+ */
