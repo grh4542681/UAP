dcb3a2a3d584b7fd3c0eaa9f903ccf164ed4fe29
RonghuaGao <grh454268@163.com>
2019-09-30 22:15:32 +0800

    config

---
+++ plat/config/config_return.cc
@@ -0,0 +1,9 @@
+#include "config_return.h"
+
+namespace config {
+
+ConfigRet::ECodeMapType ConfigRet::ECodeMap = {
+    { ECode::CONFIG_EBASE, "" },
+};
+
+}
--- plat/config/config_return.h
+++ plat/config/config_return.h
@@ -1,17 +1,38 @@
 #ifndef __CONFIG_RETURN_H__
 #define __CONFIG_RETURN_H__
 
+#include "return.h"
+
 namespace config {
 
-typedef enum class _CmRet: int {
-//common return val
-    SUCCESS = 0x00,
-    ERROR,
-    EMALLOC,
-    EINIT,
-    EUNKOWNERRNO,
-} CmRet;
-
+class ConfigRet : public ret::Return {
+public:
+    enum ECode{
+        CONFIG_EBASE = CONFIG_ERROR_CODE_BASE,
+        CONFIG_EGET,
+    };
+public:
+    static ECodeMapType ECodeMap;
+public:
+    ConfigRet(int err_code = 0) : ret::Return(err_code) {
+        err_code_vec_.push_back(&ConfigRet::ECodeMap);
+    }
+    ConfigRet(ConfigRet& other) : ret::Return(other) { }
+    ~ConfigRet() { };
+public:
+    ConfigRet& operator=(const int err_code) {
+        Return::operator=(err_code);
+        return *this;
+    }   
+    ConfigRet& operator=(const ConfigRet& ret) {
+        Return::operator=(ret);
+        return *this;
+    }   
+    ConfigRet& operator=(const ConfigRet&& ret) {
+        Return::operator=(ret);
+        return *this;
+    }
+};
 } // namespace cm
 
 #endif
