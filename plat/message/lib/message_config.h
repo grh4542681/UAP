ef8315ae2f63deed0736da5a06a99f53e4b0d439
RonghuaGao <grh454268@163.com>
2019-10-03 16:44:18 +0800

    message

---
+++ plat/message/lib/message_config.h
@@ -0,0 +1,12 @@
+#ifndef __MESSAGE_CONFIG_H__
+#define __MESSAGE_CONFIG_H__
+
+namespace message {
+
+class MessageConfig {
+    
+};
+
+}
+
+#endif
---
+++ plat/message/lib/message_package.h
@@ -0,0 +1,14 @@
+#ifndef __MESSAGE_PACKAGE_H__
+#define __MESSAGE_PACKAGE_H__
+
+namespace message {
+
+class MessagePackage {
+    
+class Id {};
+class Type {};  
+};
+
+}
+
+#endif
