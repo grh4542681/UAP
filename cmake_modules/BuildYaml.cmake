5bcae4d4f5bd61e3c87508a08ccf5f33f28d9cf0
Ronghua Gao <grh4542681@163.com>
2019-10-17 06:08:30 +0200

    submodule

---
+++ cmake_modules/BuildYaml.cmake
@@ -0,0 +1,26 @@
+message("Build ymal-cpp 3pp")
+
+include(ExternalProject)
+
+SET(YAML_DIR ${FRAM_3PP_PATH}/yaml-cpp-yaml-cpp-0.6.2)
+
+set(YAML_CMAKE_ARGS)
+list(APPEND YAML_CMAKE_ARGS "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
+list(APPEND YAML_CMAKE_ARGS "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}")
+#list(APPEND YAML_CMAKE_ARGS "-DLIBRARY_OUTPUT_PATH=${LIBRARY_OUTPUT_PATH}")
+list(APPEND YAML_CMAKE_ARGS "-DEXECUTABLE_OUTPUT_PATH=${EXECUTABLE_OUTPUT_PATH}")
+
+list(APPEND YAML_CMAKE_ARGS "-DYAML_CPP_BUILD_TESTS=OFF")
+list(APPEND YAML_CMAKE_ARGS "-DYAML_CPP_BUILD_TOOLS=OFF")
+list(APPEND YAML_CMAKE_ARGS "-DYAML_CPP_BUILD_CONTRIB=OFF")
+list(APPEND YAML_CMAKE_ARGS "-DBUILD_SHARED_LIBS=ON")
+
+ExternalProject_Add(yaml-cpp
+                    PREFIX ${FRAM_ROOT_PATH}/build
+                    SOURCE_DIR ${YAML_DIR}
+                    CONFIGURE_COMMAND cmake ${YAML_CMAKE_ARGS} ${YAML_DIR}
+                    BUILD_COMMAND $(MAKE)
+                    INSTALL_COMMAND ""
+                    )
+
+message("Build ymal-cpp 3pp done.")
